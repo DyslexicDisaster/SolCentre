ea9aa18cbd1c32cef727d380aef13b9c804cd7af
smath <smathewslynch@gmail.com>
2025-10-16 09:50:24 +0100

    Created Models Dir and Eonet Api Model

---
+++ Models/EonetModels.cs
@@ -0,0 +1,25 @@
+﻿namespace SolCentre.Models
+{
+    public class EonetModels
+    {
+        public List<EonetEvent> Events { get; set; }
+    }
+    public class EonetEvent
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Closed { get; set; }
+        public List<EonetCategory> Categories { get; set; }
+        public List<EonetGeometry> Geometries { get; set; }
+    }
+    public class EonetCategory
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+    }
+    public class EonetGeometry
+    {
+        public string Date { get; set; }
+        public List<double> Coordinates { get; set; }
+    }
+}
