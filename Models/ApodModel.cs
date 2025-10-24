21e1d6f1a4e0c97f9eb68440d952d440621279f4
smath <smathewslynch@gmail.com>
2025-10-24 09:42:16 +0100

    Created the APOD Model

---
+++ Models/ApodModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SolCentre.Models
+{
+    //Astronomy Picture of the Day (APOD)
+    public class ApodModel
+    {
+        [JsonPropertyName("date")]
+        public string Date { get; set; }
+
+        [JsonPropertyName("explanation")]
+        public string Explanation { get; set; }
+
+        [JsonPropertyName("hdurl")]
+        public string HdUrl { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+
+        [JsonPropertyName("media_type")]
+        public string MediaType { get; set; }
+
+        [JsonPropertyName("service_version")]
+        public string ServiceVersion { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+    }
+}
